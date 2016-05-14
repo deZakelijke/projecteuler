5c396c49aa78221630e7db2dca67146338bfda99
Micha de Groot <git@michadegroot.nl>
2016-05-14 14:23:18 +0200

    second problem

---
+++ 1to10/2.py
@@ -0,0 +1,20 @@
+def main():
+    # Create fibbonachi sequence
+    first = 1
+    second = 1
+    sumOfEvens = 0
+    temp = 0
+    count = 2
+    # Loop till' four million
+    while second < 4000000:
+        count += 1
+        temp = first + second 
+        first = second
+        second = temp
+        # Every third fibbonachi number is even
+        if count%3 ==0:
+            print second
+            sumOfEvens +=second
+    print sumOfEvens
+
+main()
