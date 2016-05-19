f85efa247b506e517a864880f8b0d796f95b0fd5
Micha de Groot <git@michadegroot.nl>
2016-05-19 13:03:23 +0200

    problem 7

---
+++ 1to10/7.py
@@ -0,0 +1,19 @@
+def main(n):
+    primes = [2,3]
+    # We start at five, because sqrt(5) > 2
+    integer  = 5
+    while len(primes) < n:
+        root = integer ** 0.5
+        for i in range(len(primes)):
+            # Only search untill the root of the integer
+            if primes[i] <= root:
+                if integer%primes[i] == 0:
+                    break
+        else:
+            print 'prime: ', integer
+            primes.append(integer)
+        integer += 2
+    print len(primes)
+    print primes[-1]
+
+main(10001) 
