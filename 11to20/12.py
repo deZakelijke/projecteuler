9ef8fda8f89278ef6ed8c6ceae173f676abe3f2e
Micha de Groot <git@michadegroot.nl>
2016-07-16 21:02:33 +0200

    solved 12

--- 11to20/12.py
+++ 11to20/12.py
@@ -1,13 +1,22 @@
 # Takes an int and returns all divisors of int as a list
-def divisors(num):
-    
+import math
 
+def divisors(num):
+    div = 0
+    for i in range(1, int(math.sqrt(num))+1):
+        if num%i == 0:
+            div += 2
+    return div
 
 def main():
     triangleSum = 0
-    for i in range(0,100):
+    div = 0
+    i = 1
+    while div < 501:
         triangleSum += i
         div = divisors(triangleSum)
-        print i
-        print triangleSum
+        if div > 500:
+            print(triangleSum)
+        i +=1
+
 main()
