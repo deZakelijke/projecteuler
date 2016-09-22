9f8727cc14623a86e2ea3138627097170c8d516b
deZakelijke <git@michadegroot.nl>
2016-09-22 14:14:27 +0200

    solved 15

--- 11to20/15.py
+++ 11to20/15.py
@@ -1,1 +1,16 @@
-# Derp, no iea just yet
+# use the fact that there is a correlation between the number of possibilities and the pyramid of pascal
+import numpy as np
+
+
+def pascal(n):
+    line = [1]
+    for k in range(n):
+        line.append(line[k] * (n-k) / (k+1))
+    return line
+
+def main(n):
+    pascalRow = np.array(pascal(n))
+    possibilities = sum(pascalRow*pascalRow)
+    print(possibilities)
+
+main(20)
