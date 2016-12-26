c1144ccb933f7031853607f8839133f61214bd08
deZakelijke <git@michadegroot.nl>
2016-12-26 15:12:14 +0100

    Solved 42

--- 01to50/42.py
+++ 01to50/42.py
@@ -1,17 +1,37 @@
 
 
 def addTriangleNumber(triangleList):
-    n = len(triangleList)+1
-    newTriangle = n*n(n+1)/2
-    triangleList.append(newTrinagle)
+    n = len(triangleList)
+    newTriangle = n*(n+1)//2
+    triangleList.append(newTriangle)
     return triangleList
 
 def readFile():
     f = open('42.txt')
     words = f.readline()
     words = words.split(',')
-    print(words)
+    for i in range(len(words)):
+        words[i] = words[i].rstrip('"').lstrip('"')
+    words[-1] = words[-1].rstrip('\n')
+    return words
 
+def sumOfWord(word, anchor):
+    wordSum = 0
+    for char in word:
+        wordSum += (ord(char) - anchor + 1)
+    return wordSum
 
+def main():
+    words = readFile()
+    triangleList = [0]
+    triangleWordCount = 0
+    anchor = ord(words[0])
+    for word in words:
+        wordSum = sumOfWord(word,anchor)
+        while triangleList[-1] < wordSum:
+            triangleList = addTriangleNumber(triangleList)
+        if wordSum in triangleList:
+            triangleWordCount += 1
+    print(triangleWordCount)
 
-readFile()
+main()
