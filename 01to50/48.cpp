e5fa55a54700a41d379867607a34e84ec75e9915
deZakelijke <git@michadegroot.nl>
2017-03-23 15:08:21 +0100

    Solved 48

--- 01to50/48.cpp
+++ 01to50/48.cpp
@@ -6,15 +6,19 @@ using namespace std;
 int main(){
 
     unsigned long long int counter = 0;
-    long long mod_number = 1000000000;
-    int number;
+    long long mod_number = 10000000000;
+    long long  number;
 
     for (int i = 1; i <= 1000; i++){
-        number = pow(i, i);
-        printf("Number : %d\n", number);
-        counter += number % mod_number;
-        printf("counter: %llu\n", counter);
+        number = 1;
+        for (int j = 1; j <=i; j++){
+            number *= i;
+            number = number % mod_number;
+        }
+        counter += number;
+        counter = counter % mod_number;
     }
+    printf("%llu\n", counter);
 
     return 0;
 }
