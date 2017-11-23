3a9d1414414b8756d831c87842ad423bfc7477da
Micha de Groot <michaalexanderdegroot@gmail.com>
2017-11-23 23:04:41 +0100

    started on 67

---
+++ 51to100/67.py
@@ -0,0 +1,26 @@
+import sys
+import numpy as np
+
+def read():
+    pyramid = [] 
+    input_file = sys.stdin.read().split('\n')
+    for line in input_file:
+        try:
+            pyramid.append(list(map(int, line.split(' '))))
+        except ValueError:
+            continue
+    return pyramid
+
+
+pyramid = read()
+
+for i in range(len(pyramid)-2, 0, -1):
+    #print(pyramid[i-1])
+    for j in range(len(pyramid[i-1])):
+        max_val = max(pyramid[i][j], pyramid[i][j+1])
+        pyramid[i-1][j] += max_val
+    #print(pyramid[i-1])
+
+print(pyramid[-1])
+print(pyramid[0][0])
+
