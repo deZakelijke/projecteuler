0c353add4e2b06c1488e60f944d860014c303630
Micha de Groot <michaalexanderdegroot@gmail.com>
2017-11-24 15:19:41 +0100

    Done 67

--- 51to100/67.py
+++ 51to100/67.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import pprint
 
 def read():
     pyramid = [] 
@@ -13,14 +14,16 @@ def read():
 
 
 pyramid = read()
+print(pyramid)
 
-for i in range(len(pyramid)-2, 0, -1):
+
+for i in range(len(pyramid)-1, 0, -1):
     #print(pyramid[i-1])
     for j in range(len(pyramid[i-1])):
         max_val = max(pyramid[i][j], pyramid[i][j+1])
         pyramid[i-1][j] += max_val
-    #print(pyramid[i-1])
+    print(pyramid[i-1])
 
-print(pyramid[-1])
+#print(pyramid[-1])
 print(pyramid[0][0])
 
