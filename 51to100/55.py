84fb0bdf4d17bc20667ff5c682234d06864cf50d
Micha de Groot <michaalexanderdegroot@gmail.com>
2017-11-27 18:08:25 +0100

    Done 55

--- 51to100/55.py
+++ 51to100/55.py
@@ -1,18 +1,17 @@
 import math
 
 def palindrome(n):
-    return str(n) == str(n)[::-1]
+    return int(str(n)) == int(str(n)[::-1])
 
 def lychrel(n):
     for i in range(50):
-       if palindrome(n):
-           print(n, i)
-           return True
        n = n + int(str(n)[::-1]) 
-    return False
+       if palindrome(n):
+           return False
+    return True
 
 val = 0
-for n in range(1, 10000):
+for n in range(10000):
     if lychrel(n):
         val += 1
 
